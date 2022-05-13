b2cecc235f3dfaedbab1b8fa1e37e8ee02aa1b3e
Jeikhun <104448538+Jeikhun@users.noreply.github.com>
2022-05-13 17:11:52 +0400

    Add files via upload

---
+++ 5_13/task2.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (ExamPoint() < 60)
+            {
+                    Console.WriteLine("Mezun Olmadınız ");
+            }
+            if (ExamPoint() > 60) //result75>60
+            {
+                    Console.WriteLine("Mezun Oldunuz");
+            }
+        }
+
+        static int ExamPoint()
+        {
+            int[] arr = { 60, 70, 57, 98, 75, 90 };
+            int sum = 0;
+            int counter = 0;
+            int result = 0;
+            foreach (int element in arr) 
+            {
+                sum += element;
+                counter++;
+            }
+            return result=sum/counter; //result=75
+        }
+    }
+}
