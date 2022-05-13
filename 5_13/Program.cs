871dd1a9a9be31315f182a5bc4dd2faab21dccf5
Jeikhun <104448538+Jeikhun@users.noreply.github.com>
2022-05-13 19:00:20 +0400

    Add files via upload

---
+++ 5_13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine( IsPrime(2));
+        }
+        
+
+
+        static bool IsPrime(int a)
+        {
+            bool flag = false;
+            if(a%a==0 && a % 1 == 0)
+            {
+                flag = true;
+            }
+            if (a % 2 == 0 && a>2 || a % 3 == 0 && a>3)
+            {
+                flag = false;
+            }return flag;
+
+
+
+        }
+    }
+}
