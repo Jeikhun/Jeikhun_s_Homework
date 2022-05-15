7fe15ec4ffd1976d0af734b60656809f924696cd
Jeikhun <104448538+Jeikhun@users.noreply.github.com>
2022-05-15 19:55:28 +0400

    Add files via upload

---
+++ 05_15/task2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               Console.WriteLine(PrintStar(4,6,7,9));
+        }
+
+        static string PrintStar(params int[] arr )
+        {
+            int sum=0;
+            for(int i= 0; i < arr.Length; i++)
+            {
+                sum+=arr[i];
+            }
+            if (sum % 2 == 0)
+            {
+                return "Cem 2e bolunur";
+            }
+            else
+            {
+                return "cem 2e bolunmur";
+            }           
+        }
+    }
+}
---
+++ 05_15/task3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintStar(6);
+        }
+        static void PrintStar(int hundurluk)
+        {
+           
+            for(int i = 0; i < hundurluk; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
