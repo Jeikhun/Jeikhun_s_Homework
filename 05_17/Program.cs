7a4b31e2264495e7c17294efadb5ceb8f2530831
Jeikhun <104448538+Jeikhun@users.noreply.github.com>
2022-05-17 18:47:10 +0400

    Add files via upload

---
+++ 05_17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nurlan Faiq Emil Tural Yusif";
+            Console.WriteLine(Revers(name));
+        }
+
+        static string Revers(string name)
+        {
+            string reversedName = "";
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reversedName += name[i];
+
+            }
+            return reversedName;
+        }
+    }
+}
