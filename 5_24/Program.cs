8bd2cce835faab0fa46586962c79b03d3e005d12
Jeikhun <104448538+Jeikhun@users.noreply.github.com>
2022-05-24 15:26:36 +0400

    Add files via upload

---
+++ 5_24/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Jeikhun = new Person("Jeikhun", "Jalilov", 20);
+            Console.WriteLine(Jeikhun.IsAdult);
+        }
+    }
+}
