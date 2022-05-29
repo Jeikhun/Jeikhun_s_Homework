fb92a1b3e3ba45f53bf2509eed14af2ca6fb1613
Jeikhun <104448538+Jeikhun@users.noreply.github.com>
2022-05-29 05:45:54 +0400

    Update User.cs

--- 05.25/User.cs
+++ 05.25/User.cs
@@ -1,27 +1,20 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp13
 {
-    public class User
+    internal class User
     {
         public string Name;
         public string Surname;
         public string Username;
         public string Password;
-        public User(string name, string surname, string username, string password)
+        public virtual void PrintGrade()
         {
-            Name = name;
-            Surname = surname;
-            Username = username;
-            Password = password;
-        }
-        public virtual void WhoIs()
-        {
-            Console.WriteLine("Bu istifadeci sagird deyil");
+            Console.WriteLine("Siz telebe deyilsiniz");
         }
     }
 }
