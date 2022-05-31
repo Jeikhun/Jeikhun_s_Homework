5a86a3bf36bf45eb30b88756b63c7a5678c17c77
Jeikhun <104448538+Jeikhun@users.noreply.github.com>
2022-05-31 06:01:32 +0400

    Add files via upload

---
+++ 5_26/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Jeikhun = new Student();
+
+            Jeikhun.SetGrade(new int[] { 1, 2, 3, 4, 6 });
+
+        }
+    }
+}
---
+++ 5_26/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Student
+    {
+        public void SetGrade(int[] grade)
+        {
+            foreach (var item in grade)
+            {
+                if (item % 2 == 0)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
