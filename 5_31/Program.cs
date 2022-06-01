c3b1bf6cb62fe608268f325626adb0744182d1fc
Jeikhun <104448538+Jeikhun@users.noreply.github.com>
2022-06-01 15:54:10 +0400

    Add files via upload

---
+++ 5_31/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student tamerlan = new Student();
+            Console.WriteLine(tamerlan.Age);
+            Student faiq = new Student();
+            Console.WriteLine(faiq.Age);
+            Student tural = new Student();
+            Console.WriteLine(tural.Age);
+            Student emil = new Student();
+            Console.WriteLine(emil.Age);
+        }
+    }
+}
---
+++ 5_31/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Student
+    {
+        public int Age;
+        public static int _age;
+        public Student()
+        {
+            _age++;
+            Age = _age;
+
+        }
+    }
+}
