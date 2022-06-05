641c4cdaff2055bce413511bbd9a2c503a6b4844
Jeikhun <104448538+Jeikhun@users.noreply.github.com>
2022-06-05 04:25:47 +0400

    Add files via upload

---
+++ 6_2/program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+           Print(a);
+        }
+        public static void Print(Object o)
+        {
+            Console.WriteLine(o);
+        }
+    }
+}
